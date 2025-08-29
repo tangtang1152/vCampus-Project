1eb5379fbbbdfa4adc8319420db0e14f1b466acc
Bing <Bing@tcystation>
2025-08-30 00:39:51 +0800

    feat: initial project setup with main class

---
+++ src/com/vcampus/main/Main.java
@@ -0,0 +1,13 @@
+package com.vcampus.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, vCampus! 🎉");
+        System.out.println("Project setup is successful!");
+        // 验证JDK版本
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+	}
+
+}
